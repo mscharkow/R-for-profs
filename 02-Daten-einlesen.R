8e1e3a164a664c1086e215c0a64c3deda2b19ef8
Michael Scharkow <michael@underused.org>
2022-05-18 12:53:05 +0200

    More stuff.

--- 02-Daten-einlesen.R
+++ 02-Daten-einlesen.R
@@ -8,20 +8,27 @@ audio <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday
 audio
 
 # Gepackte Dateien (zip, gz, bz2, xz) auch ohne Entpacken
+# Daten: https://osf.io/uzca3/
+
 readr::read_csv2("data/usenews.reutersdni.2019.csv.xz")
 reuters = readr::read_csv2("data/usenews.reutersdni.2019.csv.xz")
 reuters
 
 # SPSS und STATA Dateien mit haven  (Teil von Tidyverse)
+# Daten: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0155112
+
 haven::read_dta("data/pone.0155112.s001.dta")
 bos_etal = haven::read_dta("data/pone.0155112.s001.dta")
 bos_etal
 
+# Daten: https://osf.io/3byhz/
 haven::read_sav("data/dataset_IM.sav")
 im = haven::read_sav("data/dataset_IM.sav")
 im
 
 # Excel mit readxl (Teil von Tidyverse)
+
+# Daten: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab_aktuell.xlsx
 readxl::read_excel("data/Fallzahlen_Kum_Tab_aktuell.xlsx")
 rki  = readxl::read_excel("data/Fallzahlen_Kum_Tab_aktuell.xlsx", sheet = 3, skip = 3)
 rki
